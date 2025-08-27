bb9904990bda36e9495f51451953b028bdbc389e
Lukas Sabaliauskas <lukas_sabaliauskas@hotmail.com>
2025-08-27 19:36:22 +0200

    fix: disable use_single_layer, refactor: get_render_nodes

--- src/op.py
+++ src/op.py
@@ -14,8 +14,18 @@ def get_render_node_scene_layer(node: CompositorNodeRLayers):
     return (node.scene, layer)
 
 
+def get_render_nodes(unmuted_only=False):
+    node_tree =  bpy.context.scene.node_tree
+    if not node_tree:
+        return []
+    if unmuted_only:
+        return [node for node in node_tree.nodes if node.type == "R_LAYERS" and not node.mute]
+    return [node for node in node_tree.nodes if node.type == "R_LAYERS"]
+
+
 def use_view_layers(layers: list[ViewLayer], include=True):
     #TODO not really readable
+    bpy.context.scene.render.use_single_layer = False
     nb_view_layers = 0
     for scene in bpy.data.scenes:
         scene: Scene
@@ -47,13 +57,11 @@ class UFRP_OP_batch(Operator):
 
     def execute(self, context: Context):
         if self.state == True:
-            context.scene.render.use_single_layer = False
-        if self.state == True:
             nb = use_view_layers([], include=False)
         else:
             nb = use_view_layers([], include=True)
-        nodes = [node for node in context.scene.node_tree.nodes if node.type == "R_LAYERS"]
-        for node in nodes:
+        render_nodes = get_render_nodes()
+        for node in render_nodes:
             node: CompositorNodeRLayers
             node.mute = not self.state
         if self.state == True:
@@ -71,7 +79,7 @@ class UFRP_OP_OnlyUnmuted(Operator):
     
     def execute(self, context: Context):
         # get layers to use
-        render_nodes = [node for node in context.scene.node_tree.nodes if node.type == "R_LAYERS" and not node.mute]
+        render_nodes = get_render_nodes(unmuted_only=True)
         layers_to_use = []
         for node in render_nodes:
             scene, layer = get_render_node_scene_layer(node)
@@ -97,7 +105,7 @@ class UFRP_OP_OnlySelected(Operator):
         return len(render_layer_nodes) > 0
     
     def execute(self, context: Context):
-        render_nodes = [node for node in context.scene.node_tree.nodes if node.type == "R_LAYERS"]
+        render_nodes = get_render_nodes()
         selected_nodes = [node for node in context.selected_nodes if node.type == "R_LAYERS"]
         if not selected_nodes:
             self.report({"ERROR"}, "Must select at least one render layer node")
